28f20859dc76f0a0a8f01400b5659a71007c3527
Brendan Manning <tuj97064@temple.edu>
2019-01-18 17:33:43 -0500

    Deleted

---
+++ src/leads/ProfileLead/ProfileLead.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ *                              Temple Hack4Impact
+ *                                 Kiva CrowdVet
+ * 
+ *                               Lead: ProfileLead
+ * 
+ *      When the user isn't reviewing an organization, this is the default
+ * 
+ *  
+ * Props: 
+ *  backgroundImage
+ *  title
+ *  subtitle
+ */
+
+import React, { Component } from 'react';
+import './OrganizationLead.css';
+
+
+class OrganizationLead extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id="lead-content" class="lead-content" style={{backgroundImage: "url(" + this.props.backgroundImage + ")"}}>
+                <div class="lead-content-titles">
+                    <span class="lead-content-title">{this.props.title}</span>
+                    <span class="lead-content-subtitle">{this.props.subtitle}</span>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default OrganizationLead;
