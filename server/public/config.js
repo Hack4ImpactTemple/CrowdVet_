a5c90e2e2313ef4ca6431477e8a220b4b2394e52
Brendan Manning <tuj97064@temple.edu>
2019-02-21 00:38:28 -0500

    Update config.js

--- server/public/config.js
+++ server/public/config.js
@@ -1,52 +1,65 @@
+ /*
+  * 
+  * 
+  * WARNING: THIS FILE IS BEING SERVED TO THE PUBLIC
+  * 
+  * To resolve some issues with code sharing, this 
+  * config file will be made public at <serverurl>/config.js
+  * 
+  * For that reason, you must NOT PUT ANY SENSISITIVE INFO 
+  * in this file.
+  * 
+  * The endpoints are okay because they are either already known (as
+  * in the case of Kiva's GraphQL) or the client will be making HTTP
+  * requests to it anyway (as in the case of our server)
+  * 
+  * Likewise, the data file names are okay because they depend on __dirname
+  * and will not reveal the underlying structure of our server
+  * 
+  * DO NOT PUT API KEYS OR ANYTHING OF THE LIKE HERE
+  * 
+  */
+
+  /**
+   * @class Config
+   */
 var Config = {
-    
-    /**
-     * 
-     * WARNING: THIS FILE IS BEING SERVED TO THE PUBLIC
-     * 
-     * To resolve some issues with code sharing, this 
-     * config file will be made public at <serverurl>/config.js
-     * 
-     * For that reason, you must NOT PUT ANY SENSISITIVE INFO 
-     * in this file.
-     * 
-     * The endpoints are okay because they are either already known (as
-     * in the case of Kiva's GraphQL) or the client will be making HTTP
-     * requests to it anyway (as in the case of our server)
-     * 
-     * Likewise, the data file names are okay because they depend on __dirname
-     * and will not reveal the underlying structure of our server
-     * 
-     * DO NOT PUT API KEYS OR ANYTHING OF THE LIKE HERE
-     * 
-     */
-
 
     /**
-     * @var {int} port Port to operate this service on (1-65535)
+     * @property {int} port: Port to operate this service on (1-65535)
      */
     port: (typeof window === 'undefined') ? 4567 : null,
 
     /**
-      * @var {string} serverendpoint URL where this server can be reached (trailing slash required)
+     * @property {string} serverendpoint: URL where this server can be reached (trailing slash required)
      */
     serverendpoint: 'http://localhost:4567/',
 
     /**
-     * @var {string} graphqlendpoint Kiva's GraphQL endpoint
+     * @property {string} graphqlendpoint: Kiva's GraphQL endpoint
      */
     graphqlendpoint: 'https://api.kivaws.org/graphql',
 
     /**
-     * @var {string} StageOneData Sample csv data file (part 1)
+     * @prop {string} StageOneData: Sample csv data file (part 1)
      */
-    StageOneData: (typeof window === 'undefined') ? __dirname + '/data/DSE-Loan-Inquiry-Stage-I-.csv' : null,
+    StageOneData: (typeof window === 'undefined') ? '/Users/brendanmanning/Documents/Professional/Hack4Impact/CrowdVet_/server/data/DSE-Loan-Inquiry-Stage-I-.csv' : null,
     
     /**
-     * @var {string} StageOneData Sample csv data file (part 2)
+     * @prop {string} StageOneDataPrimaryKey: Index of the primary key in this file
      */
-    StageTwoData: (typeof window === 'undefined') ? __dirname + '/data/DSE-Loan-Application-Stage-II-.csv' : null
+    StageOneDataPrimaryKey: (typeof window === 'undefined') ? 0 : -1,
+
+    /**
+     * @prop {string} StageOneData: Sample csv data file (part 2)
+     */
+    StageTwoData: (typeof window === 'undefined') ? '/Users/brendanmanning/Documents/Professional/Hack4Impact/CrowdVet_/server/data/DSE-Loan-Application-Stage-II-.csv' : null,
     
+    /**
+     * @prop {string} StageTwoDataPrimaryKey: Index of the primary key in this file
+     */
+    StageTwoDataPrimaryKey: (typeof window === 'undefined') ? 0 : -1,
+
 }
 
 if (typeof window === 'undefined') {
