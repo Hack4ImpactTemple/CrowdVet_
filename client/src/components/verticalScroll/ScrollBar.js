749943b7f5b1fe4d74024950f02d9556247e3c87
Nasir-Kasumu <naskas1234@gmail.com>
2019-02-28 13:40:34 -0500

    Update ScrollBar.js
    updated with dummy data

--- client/src/components/verticalScroll/ScrollBar.js
+++ client/src/components/verticalScroll/ScrollBar.js
@@ -2,31 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './ScrollBar.css'
 
-const ScrollBar = () => {
+const ScrollBar = (props) => {
 	return(
-		//<div style={{border:"1px solid black", width: "150px", height: "100px", overflowY: "scroll"}}>
-		// style="width:150px; height:150px;overflow:auto;padding:5px;"
 		<div id = "scrollBar" >
-			<li>Video1</li>
-			<li>Video2</li>
-			<li>Video3</li>
-			<li>Video4</li>
-			<li>Video1</li>
-			<li>Video2</li>
-			<li>Video3</li>
-			<li>Video4</li>
-			<li>Video1</li>
-			<li>Video2</li>
-			<li>Video3</li>
-			<li>Video4</li>
-			<li>Video1</li>
-			<li>Video2</li>
-			<li>Video3</li>
-			<li>Video4</li>
-			<li>Video1</li>
-			<li>Video2</li>
-			<li>Video3</li>
-			<li>Video4</li>
+			<li>Problem: The Missing Middle</li>
+			<li>Kiva DSE</li>
+			<li>Introduction to Crowdvetting</li>
+			<li>How does Crowdvet work</li>
+			<li>Walkthrough</li>
+			<li>Social Impact Scale</li>
+			<li>Business Model Scale</li>
+			<li>Data</li>
+			<li>Data</li>
+			<li>Data</li>
 		</div>
 	);
 };
