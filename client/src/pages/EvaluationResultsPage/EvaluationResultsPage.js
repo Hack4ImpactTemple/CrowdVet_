66edd885d720ae71154f32c49025dd88b5abb6b7
Brendan Manning <tuj97064@temple.edu>
2019-03-15 13:02:37 -0400

    Added some data from the api

--- client/src/pages/EvaluationResultsPage/EvaluationResultsPage.js
+++ client/src/pages/EvaluationResultsPage/EvaluationResultsPage.js
@@ -134,7 +134,7 @@ class EvaluationResultsPage extends Component {
 }
 
 class EvaluationResultsPageBuilder extends CVPageBuilder {
-  
+
   // @override
   async onPageLoad(url) {
     var request = new window.APIRequest();
@@ -145,22 +145,22 @@ class EvaluationResultsPageBuilder extends CVPageBuilder {
     }
 
     // Preprocessing: Convert to a Loan object
-    var loan = new window.Loan();
-    loan.bind(json);
+    this.loan = new window.Loan();
+    this.loan.bind(json);
 
-    alert(JSON.stringify(loan));
   }
 
   pageContent() {
     return <EvaluationResultsPage 
+    loanApproved={this.loan['voting']['kiva_decision'] == "Approved"}
     evaluations={[
         {
             prompt: "1. Overall, the enterprise has a meaningful impact on low income or excluded communities [strongly disagree - strongly agree]",
             description: "5 : The social impact model of this company makes sense, and is being measured clearly and methodically.",
             votes: {
-                kiva: 5,
+                kiva: this.loan['voting']['kiva_impact'],
                 user: 5,
-                average: 3.65,
+                average: this.loan['voting']['impact'],
                 distribution: [0,1,6,9,1,0]
             } 
         },
@@ -168,9 +168,9 @@ class EvaluationResultsPageBuilder extends CVPageBuilder {
             prompt: "2. Overall, the enterprise has a viable business model [strongly disagree - strongly agree]",
             description: "4 : This company is on the road to profitability - the business model has clear potential, it seems the only barrier is a current lack of working capital.",
             votes: {
-                kiva: 4,
+                kiva: this.loan['voting']['kiva_business'],
                 user: 4,
-                average: 3,
+                average: this.loan['voting']['business'],
                 distribution: [1,2,3,4,5,6]
             } 
         },
@@ -178,9 +178,9 @@ class EvaluationResultsPageBuilder extends CVPageBuilder {
             prompt: "3. Overall, Kiva should move forward with this application and submit this loan for crowdfunding [strongly disagree - strongly agree]",
             description: "3 : I’m not sold on this. This isn’t a clear ‘yes’ for Kiva",
             votes: {
-                kiva: 3,
+                kiva: this.loan['voting']['kiva_prioritization'],
                 user: 5,
-                average: 1.23,
+                average: this.loan['voting']['prioritization'],
                 distribution: [1,1000,100,4,5,6]
             } 
         }
